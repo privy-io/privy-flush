b302cf101628df14c84f321fcec1e7428d28316c
David Ye <dave.z.ye@gmail.com>
2022-06-14 15:13:39 -0700

    Add sign-in page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,48 @@
-import "../styles/globals.css";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import ChakraUiCssReset from "@chakra-ui/css-reset";
+import "../styles/globals.css";
+import "../styles/users.css";
+import { useSession } from "../components/session";
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />;
-    </ChakraProvider>
-  );
+  const router = useRouter();
+  const session = useSession();
+  const [initialized, setInitialized] = useState(false);
+
+  // Pages that don't require authentication must explicitly define
+  // a static prop called `protected` set to `false`.
+  const pageRequiresSignedInUser = pageProps.protected !== false;
+
+  useEffect(() => {
+    function onSuccess() {
+      setInitialized(true);
+    }
+
+    function onFailure(error: Error) {
+      console.log(error);
+      setInitialized(true);
+    }
+
+    session.initialize().then(onSuccess, onFailure);
+  }, [session]);
+
+  if (!initialized) {
+    return null;
+  }
+
+  if (!session.authenticated && pageRequiresSignedInUser) {
+    router.push("/sign-in");
+    return null;
+  }
+
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
