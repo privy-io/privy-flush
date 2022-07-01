fc8ffc6487890fb9365c677383a33bcea0db89be
David Ye <dave.z.ye@gmail.com>
2022-06-30 19:47:20 -0600

    Remove extra env vars

--- config.tsx
+++ config.tsx
@@ -1,5 +1,1 @@
-export const PRIVY_API_HOST =
-  process.env.NEXT_PUBLIC_PRIVY_API_HOST || "https://api.privy.io/v0";
-export const PRIVY_KMS_HOST =
-  process.env.NEXT_PUBLIC_PRIVY_KMS_HOST || "https://api.privy.io/v0";
 export const PRIVY_API_KEY = process.env.NEXT_PUBLIC_PRIVY_API_KEY as string;
